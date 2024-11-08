8b2e9d9790875aa0445ba9a9e685129e6372077a
Jacek Graczyk <jaca173@gmail.com>
2024-11-08 09:52:16 +0100

    S01E05 Task successfully resolved with local model gemma 8b

--- common/ollama.mjs
+++ common/ollama.mjs
@@ -2,8 +2,15 @@ import axios from "axios";
 
 const URL_OLLAMA = `http://localhost:11434/api/chat`;
 
+export const MODELS = {
+  GEMMA2B: "gemma2:2b",
+  GEMMA: "gemma2",
+  LAMMA: "llama3.1",
+  LAMMA_UNCENSORED: "llama2-uncensored",
+};
+
 export class Ollama {
-  constructor(systemPrompt, model = "gemma2:2b", url = URL_OLLAMA) {
+  constructor(systemPrompt, model = MODELS.GEMMA2B, url = URL_OLLAMA) {
     this.systemContext = {
       role: "system",
       content: systemPrompt || "You are helpful ass",
@@ -13,17 +20,21 @@ export class Ollama {
   }
 
   async interact(userPrompt) {
-    const userContext = {
-      role: "user",
-      content: userPrompt,
-    };
+    try {
+      const userContext = {
+        role: "user",
+        content: userPrompt,
+      };
 
-    const completion = await axios.post(this.url, {
-      model: this.model,
-      messages: [this.systemContext, userContext],
-      stream: false,
-    });
+      const completion = await axios.post(this.url, {
+        model: this.model,
+        messages: [this.systemContext, userContext],
+        stream: false,
+      });
 
-    return completion?.data?.message?.content;
+      return completion?.data?.message?.content;
+    } catch (Error) {
+      console.log("Error Ollama message:", Error.message);
+    }
   }
 }
